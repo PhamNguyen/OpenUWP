3ce4750926a8657520a9633a5f6fc5408a702fbd
buiduc0512 <buiducwpdeveloper@outlook.com>
2016-05-25 15:23:52 +0700

    Add longToBoolJsonConvereter

---
+++ Converter/LongToBoolJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenUWP.Converter
+{
+    public class LongToBoolJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(long));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return (long)reader.Value == 1;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+        }
+    }
+}
