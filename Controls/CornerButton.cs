05f37a939c0980168ccff5a2c872e94a65844c05
nguyenvanuyn9 <nguyenvanuyn96@gmail.com>
2016-05-25 17:52:13 +0700

    Bổ sung control CornerButton - Đây là một button bình thường có thêm thuộc tính CornerRadius để làm bo góc

---
+++ Controls/CornerButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OpenUWP.Controls
+{
+    public class CornerButton : Button
+    {
+        #region CornerRadius
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CornerButton), new PropertyMetadata(new CornerRadius(0)));
+
+        public CornerRadius CornerRadius
+        {
+            set { this.SetValue(CornerRadiusProperty, value); }
+            get { return (CornerRadius)this.GetValue(CornerRadiusProperty); }
+        }
+        #endregion
+
+        public CornerButton():base()
+        {
+            this.DefaultStyleKey = typeof(CornerButton);
+        }
+    }
+}
