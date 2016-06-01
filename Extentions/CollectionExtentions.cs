309c630cbb207d7a4f873d8c9e94e3a1ec8ad502
nguyenvanuyn9 <nguyenvanuyn96@gmail.com>
2016-06-01 10:53:55 +0700

    Bổ sung CollectionExtentions.cs

---
+++ Extentions/CollectionExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenUWP.Extentions
+{
+    public static class CollectionExtentions
+    {
+        public static bool Remove<T>(this Collection<T> collection, IEnumerable<T> items)
+        {
+            bool result = true;
+            foreach (var item in items)
+            {
+                result = collection.Remove(item);
+            }
+            return result;
+        }
+    }
+}
